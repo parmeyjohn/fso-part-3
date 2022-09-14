31a0d24774c79945ce0befcc363e8ae5d126c7f4
John Cuviello <jcuviell@uci.edu>
2022-09-13 23:21:07 -0700

    finished up to 3.8

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.write(`<p>Phonebook has info for ${persons.length} people.</p>`)
+    response.write(`<p>${date}</p>`)
+    response.end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const person = req.body
+    console.log(person)
+    if (!person.name) {
+        return res.status(400).json({error: 'name missing'})
+    } else if (!person.number) {
+        return res.status(400).json({error: 'number missing'})
+    } else if (persons.find((p) => p.name === person.name)) {
+        return res.status(400).json({error: 'duplicate entry'})
+    }
+    
+    const pid = Math.random() * 1000
+    console.log(pid)
+
+    person.id = pid
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
