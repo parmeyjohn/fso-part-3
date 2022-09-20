840a599bfef4a8190ce8a99caceb5628100e59f9
John Cuviello <jcuviell@uci.edu>
2022-09-20 16:20:15 -0700

    added static frontend

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const static = require('static')
+
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(express.static('build'))
 
 let persons = [
     { 
